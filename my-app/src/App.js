849cbd55e47c6e44c79c0f915312a5ad807e77ac
rahmamhb <mihoubrahma@gmail.com>
2023-03-12 17:28:27 +0100

    setting the environment

---
+++ my-app/src/App.js
@@ -0,0 +1,11 @@
+const App = () => {
+    return ( 
+            <div>
+                <div> hello project </div>
+                <p> hello project </p>
+            </div>
+          
+     );
+}
+ 
+export default App;
