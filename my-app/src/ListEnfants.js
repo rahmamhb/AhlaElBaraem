403b1648edb8cf2c1d2d8a9ab60b70eb6663ee49
rahmamhb <mihoubrahma@gmail.com>
2023-03-29 00:27:38 +0100

    nourrice page updates it lacks only the forms

--- my-app/src/ListEnfants.js
+++ my-app/src/ListEnfants.js
@@ -2,29 +2,30 @@ import Moment from "react-moment"
 import "./ListEnfant.css"
 import Pagination from "./Pagination"
 import { useEffect, useState } from "react"
+import ChevronLeft from '@mui/icons-material/ChevronLeftRounded';
 
 const ListEnfant = () => {
 
-    let data = [{id:1,childName:"Matty",ParentEmail:"mbrydie0@skype.com",LastEditDate:new Date()},
-    {id:2,childName:"Rayna",ParentEmail:"rburgwyn1@webnode.com",LastEditDate:new Date()},
-    {id:3,childName:"Engelbert",ParentEmail:"edarlow2@hugedomains.com",LastEditDate:new Date()},
-    {id:4,childName:"Culver",ParentEmail:"cdulinty3@weibo.com",LastEditDate:new Date()},
-    {id:5,childName:"Nate",ParentEmail:"nreason4@shareasale.com",LastEditDate:new Date()},
-    {id:6,childName:"Dav",ParentEmail:"dyanyshev5@forbes.com",LastEditDate:new Date()},
-    {id:7,childName:"Lorry",ParentEmail:"lpoints6@comcast.net",LastEditDate:new Date()},
-    {id:8,childName:"Hank",ParentEmail:"hcolgan7@blogger.com",LastEditDate:new Date()},
-    {id:9,childName:"Glennie",ParentEmail:"gfobidge8@homestead.com",LastEditDate:new Date()},
-    {id:10,childName:"Bethany",ParentEmail:"bwoodwing9@mysql.com",LastEditDate:new Date()},
-    {id:11,childName:"Giselle",ParentEmail:"gpovera@house.gov",LastEditDate:new Date()},
-    {id:12,childName:"Nonnah",ParentEmail:"nvlasinb@ucla.edu",LastEditDate:new Date()},
-    {id:13,childName:"Johnny",ParentEmail:"jkaiserc@webnode.com",LastEditDate:new Date()},
-    {id:14,childName:"Ingmar",ParentEmail:"iwriterd@jiathis.com",LastEditDate:new Date()},
-    {id:15,childName:"Eloise",ParentEmail:"ewinsparee@xinhuanet.com",LastEditDate:new Date()},
-    {id:16,childName:"Osbourne",ParentEmail:"olepiscopiof@upenn.edu",LastEditDate:new Date()},
-    {id:17,childName:"Mariquilla",ParentEmail:"mfilinkovg@hatena.ne.jp",LastEditDate:new Date()},
-    {id:18,childName:"Carlie",ParentEmail:"chumphrish@rambler.ru",LastEditDate:new Date()},
-    {id:19,childName:"Danica",ParentEmail:"dlimericki@youtube.com",LastEditDate:new Date()},
-    {id:20,childName:"Elmore",ParentEmail:"eflodej@independent.co.uk",LastEditDate:new Date()}]
+    let data = [{id:1,childName:"Matty",ParentEmail:"mbrydie0@skype.com",LastEditDate:new Date(),age : 3 ,childImg:"https://image.cnbcfm.com/api/v1/image/107081378-1656361235570-GettyImages-1225403728_2.jpg?v=1656361293&w=740&h=416&ffmt=webp&vtcrop=y"},
+    {id:2,childName:"Rayna",ParentEmail:"rburgwyn1@webnode.com",LastEditDate:new Date(),age : 3,childImg:"https://image.cnbcfm.com/api/v1/image/107081378-1656361235570-GettyImages-1225403728_2.jpg?v=1656361293&w=740&h=416&ffmt=webp&vtcrop=y"},
+    {id:3,childName:"Engelbert",ParentEmail:"edarlow2@hugedomains.com",LastEditDate:new Date(),age : 3,childImg:"https://image.cnbcfm.com/api/v1/image/107081378-1656361235570-GettyImages-1225403728_2.jpg?v=1656361293&w=740&h=416&ffmt=webp&vtcrop=y"},
+    {id:4,childName:"Culver",ParentEmail:"cdulinty3@weibo.com",LastEditDate:new Date(),age : 3,childImg:"https://image.cnbcfm.com/api/v1/image/107081378-1656361235570-GettyImages-1225403728_2.jpg?v=1656361293&w=740&h=416&ffmt=webp&vtcrop=y"},
+    {id:5,childName:"Nate",ParentEmail:"nreason4@shareasale.com",LastEditDate:new Date(),age : 3,childImg:"https://image.cnbcfm.com/api/v1/image/107081378-1656361235570-GettyImages-1225403728_2.jpg?v=1656361293&w=740&h=416&ffmt=webp&vtcrop=y"},
+    {id:6,childName:"Dav",ParentEmail:"dyanyshev5@forbes.com",LastEditDate:new Date(),age : 3,childImg:"https://image.cnbcfm.com/api/v1/image/107081378-1656361235570-GettyImages-1225403728_2.jpg?v=1656361293&w=740&h=416&ffmt=webp&vtcrop=y"},
+    {id:7,childName:"Lorry",ParentEmail:"lpoints6@comcast.net",LastEditDate:new Date(),age : 3,childImg:"https://image.cnbcfm.com/api/v1/image/107081378-1656361235570-GettyImages-1225403728_2.jpg?v=1656361293&w=740&h=416&ffmt=webp&vtcrop=y"},
+    {id:8,childName:"Hank",ParentEmail:"hcolgan7@blogger.com",LastEditDate:new Date(),age : 3,childImg:"https://image.cnbcfm.com/api/v1/image/107081378-1656361235570-GettyImages-1225403728_2.jpg?v=1656361293&w=740&h=416&ffmt=webp&vtcrop=y"},
+    {id:9,childName:"Glennie",ParentEmail:"gfobidge8@homestead.com",LastEditDate:new Date(),age : 3,childImg:"https://image.cnbcfm.com/api/v1/image/107081378-1656361235570-GettyImages-1225403728_2.jpg?v=1656361293&w=740&h=416&ffmt=webp&vtcrop=y"},
+    {id:10,childName:"Bethany",ParentEmail:"bwoodwing9@mysql.com",LastEditDate:new Date(),age : 3,childImg:"https://image.cnbcfm.com/api/v1/image/107081378-1656361235570-GettyImages-1225403728_2.jpg?v=1656361293&w=740&h=416&ffmt=webp&vtcrop=y"},
+    {id:11,childName:"Giselle",ParentEmail:"gpovera@house.gov",LastEditDate:new Date(),age : 3,childImg:"https://image.cnbcfm.com/api/v1/image/107081378-1656361235570-GettyImages-1225403728_2.jpg?v=1656361293&w=740&h=416&ffmt=webp&vtcrop=y"},
+    {id:12,childName:"Nonnah",ParentEmail:"nvlasinb@ucla.edu",LastEditDate:new Date(),age : 3,childImg:"https://image.cnbcfm.com/api/v1/image/107081378-1656361235570-GettyImages-1225403728_2.jpg?v=1656361293&w=740&h=416&ffmt=webp&vtcrop=y"},
+    {id:13,childName:"Johnny",ParentEmail:"jkaiserc@webnode.com",LastEditDate:new Date(),age : 3,childImg:"https://image.cnbcfm.com/api/v1/image/107081378-1656361235570-GettyImages-1225403728_2.jpg?v=1656361293&w=740&h=416&ffmt=webp&vtcrop=y"},
+    {id:14,childName:"Ingmar",ParentEmail:"iwriterd@jiathis.com",LastEditDate:new Date(),age : 3,childImg:"https://image.cnbcfm.com/api/v1/image/107081378-1656361235570-GettyImages-1225403728_2.jpg?v=1656361293&w=740&h=416&ffmt=webp&vtcrop=y"},
+    {id:15,childName:"Eloise",ParentEmail:"ewinsparee@xinhuanet.com",LastEditDate:new Date(),age : 3,childImg:"https://image.cnbcfm.com/api/v1/image/107081378-1656361235570-GettyImages-1225403728_2.jpg?v=1656361293&w=740&h=416&ffmt=webp&vtcrop=y"},
+    {id:16,childName:"Osbourne",ParentEmail:"olepiscopiof@upenn.edu",LastEditDate:new Date(),age : 3,childImg:"https://image.cnbcfm.com/api/v1/image/107081378-1656361235570-GettyImages-1225403728_2.jpg?v=1656361293&w=740&h=416&ffmt=webp&vtcrop=y"},
+    {id:17,childName:"Mariquilla",ParentEmail:"mfilinkovg@hatena.ne.jp",LastEditDate:new Date(),age : 3,childImg:"https://image.cnbcfm.com/api/v1/image/107081378-1656361235570-GettyImages-1225403728_2.jpg?v=1656361293&w=740&h=416&ffmt=webp&vtcrop=y"},
+    {id:18,childName:"Carlie",ParentEmail:"chumphrish@rambler.ru",LastEditDate:new Date(),age : 3,childImg:"https://image.cnbcfm.com/api/v1/image/107081378-1656361235570-GettyImages-1225403728_2.jpg?v=1656361293&w=740&h=416&ffmt=webp&vtcrop=y"},
+    {id:19,childName:"Danica",ParentEmail:"dlimericki@youtube.com",LastEditDate:new Date(),age : 3,childImg:"https://image.cnbcfm.com/api/v1/image/107081378-1656361235570-GettyImages-1225403728_2.jpg?v=1656361293&w=740&h=416&ffmt=webp&vtcrop=y"},
+    {id:20,childName:"Elmore",ParentEmail:"eflodej@independent.co.uk",LastEditDate:new Date(),age : 3,childImg:"https://image.cnbcfm.com/api/v1/image/107081378-1656361235570-GettyImages-1225403728_2.jpg?v=1656361293&w=740&h=416&ffmt=webp&vtcrop=y"}]
     useEffect(()=>{
     }, []);
     function setRowNb(nbr){
@@ -37,18 +38,57 @@ const ListEnfant = () => {
     const indexOfFirstChild = indexOfLastChild - childrenPerPage ;
     const currentChild = data.slice(indexOfFirstChild,indexOfLastChild);
     const paginate = (pageNumber) => setCurrentPage(pageNumber) ;
+    const [currentOverlayIndex, setCurrentOverlayIndex] = useState(null);
+    const emojis = ["😒","🙁","😊","🥰","👏"];
     return ( 
+        
         <div className="list-page">
             <div className="list-enfant-container" onChange={setRowNb(currentChild.length)}>
                 {currentChild.map((item,index)=>{
                     return(
-                        <div className="enfant-info" key={index} >
-                            <span className="nbr"> {index + 1}</span>
-                            <span className="child-name"> {item.childName}</span>
-                            <span className="parent-email">{item.ParentEmail}</span>
-                            <span className="last-edit"><Moment format='lll'>{item.LastEditDate}</Moment></span>
-                            <button>éditer</button>
+                        <div> 
+                            <div  className={currentOverlayIndex === index ? "comportement-overlay open" : "comportement-overlay"}  key={index}>
+                                <div className="comportement-container">
+                                    <span className="comportement-header">
+                                        <button onClick={() => setCurrentOverlayIndex(null)}><ChevronLeft></ChevronLeft></button>
+                                        <div className="about-enfant">
+                                            <img src={item.childImg} alt="childImg"></img>
+                                            <div className="text">
+                                                <p>{item.childName} , {item.age} ans</p>
+                                                <p>{item.ParentEmail}</p>
+                                            </div>
+                                        </div>  
+                                    </span>
+                                    <form>
+                                        <div className="comportement-of-the-day">
+                                            <h3>Comportement d’aujourd'hui</h3>
+                                            <div class="container">
+                                                {emojis.map((emoji,index)=>{
+                                                    return(
+                                                        <div class="item">
+                                                            <label for={index}>
+                                                                <input class="radio" type="radio" name="feedback" id={index} value={index}/>
+                                                                <span>{emoji}</span>
+                                                            </label>
+                                                        </div>
+                                                    )
+                                            })}
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>    
+                            </div>
+
+                            <div className="enfant-info" key={index} >
+                                <span className="nbr"> {index + 1}</span>
+                                <span className="child-name"> {item.childName}</span>
+                                <span className="parent-email">{item.ParentEmail}</span>
+                                <span className="last-edit"><Moment format='lll'>{item.LastEditDate}</Moment></span>
+                                <button  onClick={() => setCurrentOverlayIndex(index)} key={index}>éditer</button>
+                            </div>
+
                         </div>
+                       
                     )
                 })
                 }            
